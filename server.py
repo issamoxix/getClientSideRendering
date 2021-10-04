8daf9edb794a03f6191c75dc07332e6e7eaa038e
issamoxix <issamhaidaoui@gmail.com>
2021-10-04 13:04:45 +0000

    initial commit

---
+++ server.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from requests_html import AsyncHTMLSession
+from sele import gethtml
+
+app = FastAPI()
+
+
+@app.get('/')
+
+async def func(url:str):
+    try:
+        asession = AsyncHTMLSession()
+        r = await asession.get(url)
+        await r.html.arender(sleep=1)
+    except:
+        return 0
+        pass
+
+    return {"html":r.text}
+
+@app.get('/sele')
+
+def fk(url:str):
+    return {"html":gethtml(url)}
